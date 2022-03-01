a68dbd9d7504309721237c3c89c08f24b33ce18a
Lyptuss <100670971+Lyptuss@users.noreply.github.com>
2022-02-28 23:09:31 -0800

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
-  imageBackground: false,
+  name: 'Lee',
+  imageBackground: true,
   openInNewTab: true,
   twelveHourFormat: false,
 
